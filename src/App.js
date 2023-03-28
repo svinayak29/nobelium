13d5a61f590df9c82288d2832dc8a610c514ec35
svinayak29 <shidhivinayak14@gmail.com>
2023-03-28 11:30:37 +0530

    Hooks Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text,setText] = useState("")
+  const [data,setData] = useState([])
+
+  const handleAddText = () => {
+    setData([...data,text])
+    setText("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <input value={text} onChange={(e) =>setText(e.target.value)}/>
+      <button onClick={handleAddText}>Add</button>
+     </div>
+
+     <div>
+      {
+        data && data.map((ele,i) =>(<li key={i}>{ele}</li>)
+        )
+      }
+     </div>
     </div>
   );
 }
